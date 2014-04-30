86377a4e1de1f76749a9fdac232544134eb9ba1d
danhakin <mdvillalobos@gmail.com>
2014-04-30 22:01:01 +0200

    Added css selectors and jslint to spa.js

--- js/spa.js
+++ js/spa.js
@@ -1,61 +1,25 @@
- var spa = (function($){
-     var 
-         configMap = {
-             extended_height: 434,
-             extended_title: 'Click to retract',
-             retracted_height: 16,
-             retracted_title: 'Click to extend',
-             template_html: '<div class="spa-slider"><\/div'
-         },
-         $chatSlider,
-         toggleSlider,
-         onClickSlider,
-         initModule;
-         
+/*
+ * spa.js
+ * Root namespace module
+*/
 
-     toggleSlider = function() {
-         var 
-             slider_height = $chatSlider.height();
-             
-         if (slider_height === configMap.retracted_height) {
-             $chatSlider
-               .animate({height: configMap.extended_height})
-               .attr('title', configMap.extended_title);
-             return true;
-         }
-         
-         if ( slider_height === configMap.extended_height) {
-             $chatSlider
-                .animate({height: configMap.retracted_height})
-                .attr('title', configMap.retracted_title);
-             return true;
-         }
-         
-         // Do not take action if slider is in transition
-         return false;
-         
-     };
-     
-     onClickSlider = function(event) {
-         toggleSlider();
-         return false;
-     };
-     
-     initModule = function ($container){
-         $container.html(configMap.template_html);
-         
-         $chatSlider = $container.find('.spa-slider');
-         
-         $chatSlider
-            .attr('title', configMap.retracted_title)
-            .click(onClickSlider);
-            
-         return true;
-         
-     };
-     
-     return { initModule : initModule };
-     
-     
-  }(jQuery));
- 
+/*jslint         browser : true, continue : true,
+  devel  : true,  indent : 2,      maxerr : 50,
+  newcap : true,   nomen : true, plusplus : true,
+  regexp : true,  sloppy : true,     vars : false,
+  white  : true
+*/
+
+/*global jQuery, spa*/
+
+var spa = (function ($) {
+    var initModule = function ($container) {
+        $container.html(
+            '<h1 style="display: inline-block; margin: 25px;">'
+            + 'hello world!'
+            + '</h1>'
+        );
+    };
+    
+    return { initModule : initModule };
+}());
