ba34af9dc5fa70bfc3df75a5c91de84f92bf7c41
danhakin <mdvillalobos@gmail.com>
2014-05-04 23:43:09 +0200

    Implemented toggleChat function

--- js/spa.shell.js
+++ js/spa.shell.js
@@ -16,23 +16,28 @@ spa.shell = (function () {
     /*** Module scope variables ***/
     
     var configMap = {
-        main_html : String()
-            + '<div class="spa-shell-head">'
-                + '<div class="spa-shell-head-logo"></div>' 
-                + '<div class="spa-shell-head-acct"></div>' 
-                + '<div class="spa-shell-head-search"></div>'
-            + '</div>' 
-            + '<div class="spa-shell-main">'
-                + '<div class="spa-shell-main-nav"></div>'
-                + '<div class="spa-shell-main-content"></div>' 
-            + '</div>' 
-            + '<div class="spa-shell-foot"></div>' 
-            + '<div class="spa-shell-chat"></div>' 
-            + '<div class="spa-shell-modal"></div>'
+            main_html : String()
+                + '<div class="spa-shell-head">'
+                    + '<div class="spa-shell-head-logo"></div>' 
+                    + '<div class="spa-shell-head-acct"></div>' 
+                    + '<div class="spa-shell-head-search"></div>'
+                + '</div>' 
+                + '<div class="spa-shell-main">'
+                    + '<div class="spa-shell-main-nav"></div>'
+                    + '<div class="spa-shell-main-content"></div>' 
+                + '</div>' 
+                + '<div class="spa-shell-foot"></div>' 
+                + '<div class="spa-shell-chat"></div>' 
+                + '<div class="spa-shell-modal"></div>',
+            chat_extend_time    : 1000,
+            chat_retract_time   : 300,
+            chat_extend_height  : 450,
+            chat_retract_height : 15
         },
         stateMap = { $container: null },
         jqueryMap = {},
         setJQueryMap,
+        toogleChat,
         initModule;
     
     /*** Utility methods ***/
@@ -49,7 +54,53 @@ spa.shell = (function () {
      */
     setJQueryMap = function () {
         var $container = stateMap.$container;
-        jqueryMap = { $container : $container };
+        
+        jqueryMap = { 
+            $container : $container,
+            $chat      : $container.find( '.spa-shell-chat' )
+        };
+    };
+    
+    
+    /**    
+     * Extends or retract chat slider.    
+     * @param boolean do_extend - if true, extends slider; if false retracts it     
+     * @param function callback - optional function to execute at the end of 
+     *  animation
+     * @returns boolean
+     *   true  - slider animation activated
+     *   false - slider animation not activated
+     */
+    toogleChat = function ( do_extend, callback ) {
+        var px_chat_ht = jqueryMap.$chat.height(),
+            is_open    = px_chat_ht === configMap.chat_extend_height,
+            is_closed  = px_chat_ht === configMap.chat_retract_height,
+            is_sliding = !is_open && !is_closed;
+        
+        // avoid race condition 
+        if ( is_sliding ) { return false; }
+        
+        // begin extend chat slider
+        if ( do_extend ) {
+            jqueryMap.$chat.animate( 
+                { height : configMap.chat_extend_height },
+                configMap.chat_extend_time,
+                function () {
+                    if ( callback ) { callback( jqueryMap.$chat ); }
+                }
+            );
+            return true;
+        }
+        
+        // begin retract chat slider
+        jqueryMap.$chat.animate( 
+            { height : configMap.chat_retract_height },
+            configMap.chat_retract_time, 
+            function () {
+                if ( callback ) { callback( jqueryMap.$chat ); }
+            }
+        );
+        return true;
     };
     
     /*** Event handlers ***/
@@ -57,6 +108,7 @@ spa.shell = (function () {
     /*** Public methods ***/
     
     initModule = function ( $container ) {
+        // load HTML and map jQuery collections
         stateMap.$container = $container;
         $container.html( configMap.main_html );
         setJQueryMap();
